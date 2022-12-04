f249b20d32f3172abac852798f85aa0dcebeccc8
Danny <danny.schober@gmx.de>
2022-12-04 08:45:43 +0100

    clean up 04

--- 04/aoc04_1.cpp
+++ 04/aoc04_1.cpp
@@ -38,31 +38,29 @@ int main() {
         stringstream segmentstream;
         while (getline(itemstream, segments, ',')) {
             i++;
-            cout << "segments: " << segments << endl;
+
             segmentstream.str(segments);
             segmentstream.clear();  // added this line to clear the stringstream
-            cout << "segmentstream: " << segmentstream.str() << endl;
+
             list<int> segment_vec;
-            cout << 2 << endl;
             string segment = "";
+
             while (getline(segmentstream, segment, '-')) {
-                cout << "seg: " << segment << endl;
                 segment_vec.push_back(stoi(segment));
             }
+
             set<int> range = convert_to_set(segment_vec.front(), segment_vec.back());
             segment_vec.clear();
-            for (int const& z : range)
-            {
-                std::cout << z << ' ';
-            }
-            cout << endl;
+            
             seglist.push_back(range);
             set<int> out;
-            cout << 3 << endl;
+
             if (i == 2) {
                 set_intersection(seglist.front().begin(), seglist.front().end(), seglist.back().begin(), seglist.back().end(), inserter(out, out.begin()));
+
                 if (out == seglist.front() || out == seglist.back())
                     containment_counter++;
+                    
                 seglist.clear();
                 i = 0;
             }            

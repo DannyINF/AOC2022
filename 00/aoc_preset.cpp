bafbcabdad221788a9d8855c99945ed5bdeaa935
Danny <danny.schober@gmx.de>
2022-12-07 08:21:22 +0100

    implement time stopping for future days

--- 00/aoc_preset.cpp
+++ 00/aoc_preset.cpp
@@ -2,22 +2,28 @@
 #include <fstream>
 #include <vector>
 #include <list>
+#include <chrono>
 #include <string>
 #include "../custom_functions/custom_functions.h"
 
 using namespace std;
+using namespace std::chrono;
 
 int main() {
+    auto start = high_resolution_clock::now();
 
     string item;
     ifstream myfile("aocXX.txt");
 
-    vector<int> input;
-
     while ( getline(myfile, item) ) {
-        input.push_back(stoi(item));
+        
     }
 
     // Close the file
     myfile.close();
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: "
+         << duration.count()/1000000.0 << " seconds" << endl;
 }
